48558bda79be755ac3f72e75a101cd07ec744dbd
zulfadnisa <zulfadzanisa@gmail.com>
2025-07-28 19:56:01 +0700

    monitor: bug fixing part3

--- monitor.py
+++ monitor.py
@@ -8,6 +8,18 @@ import time
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 FILENAME = "urls50.txt"
+HEADERS = {
+    "User-Agent": 
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/115.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "DNT": "1",  # Do Not Track
+    "Cache-Control": "no-cache"
+}
 
 def send_telegram(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
@@ -18,39 +30,42 @@ def send_telegram(message):
     except Exception as e:
         print(f"❌ Gagal mengirim notifikasi ke Telegram: {e}")
 
+def try_request(url):
+    delay = 2
+    retries = 1
+
+    for attempt in range(retries + 1):
+        timeoutVal = 10 if attempt == 0 else 15
+        try:
+            return requests.get(url, headers=HEADERS, timeout=timeoutVal) #30 detik kalau di uptimerobot
+        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
+            if attempt < retries:
+                time.sleep(delay)
+            else:
+                raise
+
 def check_websites(urls):
     results = []
     total_success = 0
-
-    headers = {
-        "User-Agent": 
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-            "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/115.0.0.0 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1",
-        "DNT": "1",  # Do Not Track
-        "Cache-Control": "no-cache"
-    }
+    total_timeout = 0
 
     for url in urls:
         try:
-            response = requests.get(url,headers=headers, timeout=15) #30 detik kalau di uptimerobot
+            response = try_request(url)
             status_code = response.status_code
 
             if 200 <= status_code < 400 :
                 total_success+=1
             elif status_code == 403:
-                if "cloudflare" in response.text.lower():
+                if "cloudflare" in response.text.lower() or "access denied" in response.text.lower():
                     results.append(f"❌ {url} - Bot-blocked (403)")
                 else:
                     results.append(f"❌ {url} - Akses ditolak (403)")
             else:
                 results.append(f"❌ {url} - Error ({status_code})")
         except requests.exceptions.Timeout:
-            results.append(f"❓ {url} - Timeout")
+            results.append(f"⏰ {url} - Timeout")
+            total_timeout+=1
         except requests.exceptions.ConnectionError:
             results.append(f"❓ {url} - Connection Error")
         except requests.exceptions.TooManyRedirects:
@@ -58,7 +73,7 @@ def check_websites(urls):
         except requests.exceptions.RequestException as e:
             results.append(f"⚠️ {url} - Gagal Akses ({type(e).__name__})")
 
-    return results,total_success
+    return results,total_success,total_timeout
 
 def load_urls_from_file():
     urls = []
@@ -82,7 +97,7 @@ def main():
     urls = load_urls_from_file()
     total_url = len(urls)
 
-    results,total_success = check_websites(urls)
+    results,total_success,total_timeout = check_websites(urls)
     result_msg = "\n".join(results)
     if len(results) == 0 and total_success == len(urls):
         result_msg = "\n ✅ Semua URL Berjalan/OK (200)"    
@@ -91,7 +106,7 @@ def main():
     duration = end_time - start_time
     now = datetime.now(ZoneInfo("Asia/Jakarta"))
     timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
-    message = f"🌐 Website Monitoring Result\n🕒 {timestamp}\n" + f"⏱️ Durasi: {duration:.2f} detik\n"+f"Success: {total_success}/{total_url}\n\n" + result_msg
+    message = f"📡 Website Monitoring Result\n📅 {timestamp}\n" + f"⏱️ Durasi: {duration:.2f} detik\n"+f"Aktif: {total_success}/{total_url}\n"+f"Timeout: {total_timeout}\n\n" + result_msg
     send_telegram(message)
 
 
